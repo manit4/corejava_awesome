8e29da6635bd07c46502f937595c432a89481ce7
Manit <manit.manu4@gmail.com>
2024-07-25 21:26:50 +0530

    25-07

--- spring-boot/boot_jpa/boot_jpa/src/main/java/com/example/BootJpaApplication.java
+++ spring-boot/boot_jpa/boot_jpa/src/main/java/com/example/BootJpaApplication.java
@@ -27,9 +27,82 @@ public class BootJpaApplication implements CommandLineRunner {
 //		
 //		userRepository.save(user);
 		
-		List<User> users = userRepository.findAll();
+//		List<User> users = userRepository.findAll();
+//		
+//		users.forEach(user -> System.out.println(user.getUsername()+", "+user.getName()));
+//		
+//		System.out.println("No of users are "+userRepository.count());
 		
-		users.forEach(user -> System.out.println(user.getUsername()+", "+user.getName()));
+		//User user = new User("eileen@1", "12345", "eileen21@gmail.com");
+//		
+//		userRepository.save(user);
+		
+		
+		//userRepository.deleteById("eileen@123");
+		
+		//userRepository.delete(user);
+		
+//		User user1 = new User("lau@123", "123", "Laurene");
+//		User user2 = new User("Xi@123", "234", "Xi Wang");
+//		
+//		List<User> users = new ArrayList<>();   users.add(user1);  users.add(user2);
+//		
+//		userRepository.saveAll(users);
+		
+//		Sort sort = Sort.by(Sort.Direction.DESC, "name");
+//		
+//		userRepository.findAll(sort).forEach(user -> System.out.println(user.getName()));;
+		
+		
+		
+//		Pageable pageable = Pageable.ofSize(1);
+//		
+//		Page<User> page = userRepository.findAll(pageable);
+//		
+//		System.out.println(page.getTotalPages());
+		
+//		page.forEach(user -> System.out.println(user.getName()+", "+user.getUsername()));
+		
+		
+		
+		
+//		List<User> users = userRepository.findAll();
+//		
+//		
+//		users.stream().filter( user -> {
+//			
+//			if( user.getName().equals("Xi") && user.getPassword().equals("123")) {
+//				return true;
+//			}
+//			else {
+//				return false;
+//				
+//			}
+//		}).forEach(val -> System.out.println(val));
+		
+		
+		
+		
+//		List<User> users = userRepository.findByUsernameAndPassword("lau@123", "123");
+//		
+//		users.forEach(user -> System.out.println(user.getName()+", "+user.getPassword()));
+		
+		
+//		List<User> users = userRepository.findByPasswordLessThan("12345");
+//		users.forEach(user -> System.out.println(user.getName()+", "+user.getPassword()));
+		
+		
+		
+		List<User> users = userRepository.findByPasswordLessThanEqual("12345");
+		users.forEach(user -> System.out.println(user.getName()+", "+user.getPassword()));
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- spring-boot/boot_jpa/boot_jpa/src/main/java/com/example/repository/UserRepository.java
+++ spring-boot/boot_jpa/boot_jpa/src/main/java/com/example/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,24 @@ import com.example.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
+	
+	public List<User> findByUsernameAndPassword(String username, String password);
+	
+	public List<User> findByPasswordLessThan(String password);
+	
+	public List<User> findByPasswordLessThanEqual(String password);
 
 }
+
+
+
+
+//@Repository
+//public interface UserRepository extends JpaRepository<User, String>{//Extending JPARepository will have 
+//		//more no of methods than you get by extending CrudRepository
+//
+//}
+//public interface UserRepository extends CrudRepository<User, String>{//If your Repository extends
+		//CrudRepository then you will have lesser no of methods than you get by extending JPARepository...
+//
+//}
